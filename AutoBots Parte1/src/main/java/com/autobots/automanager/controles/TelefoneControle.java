f4a1ad616900f6bdd586a6def1ef7a7a87b509f1
Rafael Waltrick <rafael.waltrick@fatec.sp.gov.br>
2022-11-10 11:50:19 -0300

    Castastro e atualizar telefone

--- AutoBots Parte1/src/main/java/com/autobots/automanager/controles/TelefoneControle.java
+++ AutoBots Parte1/src/main/java/com/autobots/automanager/controles/TelefoneControle.java
@@ -7,13 +7,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.autobots.automanager.entidades.Cliente;
 import com.autobots.automanager.entidades.Telefone;
+import com.autobots.automanager.modelo.ClienteSelecionador;
 import com.autobots.automanager.modelo.TelefoneAtualizador;
 import com.autobots.automanager.modelo.TelefoneSelecionador;
+import com.autobots.automanager.repositorios.ClienteRepositorio;
 import com.autobots.automanager.repositorios.TelefoneRepositorio;
 
 @RestController
@@ -21,6 +26,10 @@ import com.autobots.automanager.repositorios.TelefoneRepositorio;
 public class TelefoneControle {
 	@Autowired
 	private TelefoneRepositorio repositorio;
+	@Autowired
+	private ClienteRepositorio repositorioCliente;
+	@Autowired
+	private ClienteSelecionador selecionadorCliente;
 	@Autowired
 	private TelefoneSelecionador selecionador;
 	
@@ -36,13 +45,23 @@ public class TelefoneControle {
 		return selecionador.selecionar(telefone, id);
 	}
 	
-	@GetMapping("/atualizar")
+	@PutMapping("/atualizar")
 	public ResponseEntity<Telefone> atualizarCliente(@RequestBody Telefone atualizacao) {
 		Telefone telefone = repositorio.getById(atualizacao.getId());
 		TelefoneAtualizador atualizador = new TelefoneAtualizador();
 		atualizador.atualizar(telefone, atualizacao);
 		repositorio.save(telefone);
 		return new ResponseEntity<Telefone>(atualizacao, HttpStatus.OK);
+	}
+	
+	@PostMapping("/cadastro/{id}")
+	public void cadastrarTelefone(@PathVariable Long id, @RequestBody Telefone telefone) {
+		List<Cliente> cliente =  repositorioCliente.findAll();
+		Cliente alvo = selecionadorCliente.selecionar(cliente, id);
+		alvo.getTelefones().add(telefone);
+		repositorioCliente.save(alvo);
+		repositorio.save(telefone);
+		
 	}
 
 }
